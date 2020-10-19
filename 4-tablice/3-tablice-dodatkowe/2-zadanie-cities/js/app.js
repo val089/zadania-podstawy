faee741dd7c3b1226e01f3563fbe39ee342de10e
Kamil Szerląg <30775271+val089@users.noreply.github.com>
2020-10-19 11:43:54 +0200

    Update app.js
    changed "population" to "people"

--- 4-tablice/3-tablice-dodatkowe/2-zadanie-cities/js/app.js
+++ 4-tablice/3-tablice-dodatkowe/2-zadanie-cities/js/app.js
@@ -11,18 +11,18 @@ console.log(people);
 
 // ## Zadanie 3
 // Wypisz pierwsze miasto, w którym jest ponad 10000 ludzi
-const firstBig = cities.find(city => city.population >= 10000);
+const firstBig = cities.find(city => city.people >= 10000);
 console.log(firstBig);
 
 // ## Zadanie 4
 // Wypisz miasta, w których ludzi jest ponad średnią
 const avg = people / cities.length;
-const aboveAvg = cities.filter(city => city.population > avg);
+const aboveAvg = cities.filter(city => city.people > avg);
 console.log(aboveAvg);
 
 // ## Zadanie 5
 // Wypisz nazwy wszystkich miast, w których jest ponad 10000 ludzi
-const bigCities = cities.filter(city => city.population > 10000)
+const bigCities = cities.filter(city => city.people > 10000)
 const namesBigCities = bigCities.map(city => city.name);
 console.log(namesBigCities);
 
